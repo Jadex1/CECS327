e6a379c70b39824a238b06d3d3fdacb816181bbf
jadex1 <greatwolf3d@gmail.com>
2016-09-03 18:20:19 -0700

    Modifed a few things, cleaned up a few files

--- assignment1/ftpclient.cpp
+++ assignment1/ftpclient.cpp
@@ -1,14 +1,14 @@
 //Assignment 1 - FTP Client
 //CECS 327
-//Brendan McMahon
+//Brendan McMahon, James Hall, Andrew Camarena
 
-#include <iostream>    //cout
+#include <iostream>        //cout
 #include <string>
-#include <stdio.h> //printf
+#include <stdio.h>         //printf
 #include <stdlib.h>
-#include <string.h>    //strlen
+#include <string.h>        //strlen
 #include <sys/socket.h>    //socket
-#include <arpa/inet.h> //inet_addr
+#include <arpa/inet.h>     //inet_addr
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -18,10 +18,7 @@ using namespace std;
 
 #define BUFFER_LENGTH 2048
 
-
-
-int createConnection(std::string host, int port)
-{
+int createConnection(std::string host, int port) {
     int sock;
     struct sockaddr_in sockaddr;
 
@@ -31,19 +28,16 @@ int createConnection(std::string host, int port)
     sockaddr.sin_port= htons(port);
 
     int a1,a2,a3,a4;
-    if (sscanf(host.c_str(), "%d.%d.%d.%d", &a1, &a2, &a3, &a4 ) == 4)
-    {
+    if (sscanf(host.c_str(), "%d.%d.%d.%d", &a1, &a2, &a3, &a4 ) == 4) {
         cout << "by ip";
         sockaddr.sin_addr.s_addr =  inet_addr(host.c_str());
-    }
-    else {
+    } else {
         cout << "by name";
         hostent * record = gethostbyname(host.c_str());
         in_addr * addressptr = (in_addr *) record->h_addr;
         sockaddr.sin_addr = *addressptr;
     }
-    if(connect(sock,(struct sockaddr *)&sockaddr,sizeof(struct sockaddr))==-1)
-    {
+    if (connect(sock,(struct sockaddr *)&sockaddr,sizeof(struct sockaddr))==-1) {
         perror("connection fail");
         exit(1);
         return -1;
@@ -51,33 +45,29 @@ int createConnection(std::string host, int port)
     return sock;
 }
 
-std::string requestReply(int sock, std::string message)
-{
+std::string requestReply(int sock, std::string message) {
     char buffer[BUFFER_LENGTH];
     std::string reply;
     int count = send(sock, message.c_str(), message.size(), 0);
-    if (count > 0)
-    {
+    if (count > 0) {
         usleep(1000);
         do {
             count = recv(sock, buffer, BUFFER_LENGTH-1, 0);
             buffer[count] = '\0';
             reply += buffer;
-        }while (count ==  BUFFER_LENGTH-1);
+        } while (count ==  BUFFER_LENGTH-1);
     }
     return buffer;
 }
 
 
-int request(int sock, std::string message)
-{
+int request(int sock, std::string message) {
     char buffer[BUFFER_LENGTH];
     std::string reply;
     return send(sock, message.c_str(), message.size(), 0);
 }
 
-std::string reply(int sock)
-{
+std::string reply(int sock) {
 	std::string strReply;
 	int count;
 	char buffer[BUFFER_LENGTH];
@@ -89,9 +79,8 @@ std::string reply(int sock)
 	return strReply;
 }
 
-short PASV(int sockpi)
-{
-	std::string response = requestReply(sockpi, "PASV\r\n");
+short PASV(int sockpi) {
+    std::string response = requestReply(sockpi, "PASV\r\n");
 	std::string ip = response.substr(response.find('('), response.length());
 	ip.erase(std::remove(ip.begin(), ip.end(), ')'), ip.end());
 	ip.erase(std::remove(ip.begin(), ip.end(), '('), ip.end());
@@ -112,8 +101,8 @@ short PASV(int sockpi)
 	bits = bits | bit;
 	return (short)bits.to_ulong();
 }
-void LIST(int port, int sockpi)
-{
+
+void LIST(int port, int sockpi) {
 	std::string strReply;
 	int nsock;
 	nsock = createConnection("130.179.16.134", port);
@@ -123,8 +112,7 @@ void LIST(int port, int sockpi)
 	request(nsock, "CLOSE \r\n");
 	std::cout << reply(sockpi) << std::endl;
 }
-void RETR(int port, int sockpi)
-{
+void RETR(int port, int sockpi) {
 	int nsock;
 	std::string file;
 	std::cout << "Enter the name of the file (not a directory) with file extension: ";
@@ -137,29 +125,26 @@ void RETR(int port, int sockpi)
 	std::cout << reply(sockpi) << std::endl;
 
 }
-void QUIT(int nsock)
-{
+void QUIT(int nsock) {
 	request(nsock, "QUIT \r\n");
 	std::cout << reply(nsock) << std::endl;
 }
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	int sockpi;
 	std::string strReply;
 
 	//TODO  arg[1] can be a dns or an IP address using gethostbyname.
-	if (argc > 2)
-	{
+	if (argc > 2){
 		sockpi = createConnection(argv[1], atoi(argv[2]));
 	}
-	if (argc == 2)
-		sockpi = createConnection(argv[1], 21);
-	else
-		sockpi = createConnection("130.179.16.134", 21);
+	if (argc == 2) {
+        sockpi = createConnection(argv[1], 21);
+    } else {
+        sockpi = createConnection("130.179.16.134", 21);
+    }
+
 	strReply = reply(sockpi);
 	std::cout << strReply << std::endl;
-
 
 	strReply = requestReply(sockpi, "USER anonymous\r\n");
 	//TODO parse srtReply to obtain the status. Let the system act according to the status and display
@@ -170,7 +155,6 @@ int main(int argc, char *argv[])
 	std::cout << strReply << std::endl;
 	//TODO parse srtReply to obtain the status. Let the system act according to the status and display
 	// friendly user to the user
-
 	short port = PASV(sockpi);
 	LIST(port, sockpi);
 	port = PASV(sockpi);
@@ -179,11 +163,6 @@ int main(int argc, char *argv[])
 
 	system("Pause");
 
-
-
 	//TODO implement PASV, LIST, RETR
-
-
 	return 0;
 }
- 
