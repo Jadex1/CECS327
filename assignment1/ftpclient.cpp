7d2c8b719568fcf04d11af7e37c7a231ff76bc0b
yaffa7 <brendan6z@gmail.com>
2016-09-08 10:46:29 -0700

    Merge branch 'develop' into brendanBranch
    # Conflicts:
    #	assignment1/ftp
    #	assignment1/ftpclient.cpp

--- assignment1/ftpclient.cpp
+++ assignment1/ftpclient.cpp
@@ -1,6 +1,6 @@
-//Assignment 1 - FTP Client
-//CECS 327
-//Brendan McMahon
+// Assignment 1 - FTP Client
+// CECS 327
+// Brendan McMahon, James Hall, Andrew Camarena
 
 #include <iostream>    //cout
 #include <string>
@@ -18,8 +18,12 @@ using namespace std;
 
 #define BUFFER_LENGTH 2048
 
+<<<<<<< HEAD
 int createConnection(string host, int port)
 {
+=======
+int createConnection(string host, int port) {
+>>>>>>> develop
     int sock;
     struct sockaddr_in sockaddr;
 
@@ -29,18 +33,15 @@ int createConnection(string host, int port)
     sockaddr.sin_port= htons(port);
 
     int a1,a2,a3,a4;
-    if (sscanf(host.c_str(), "%d.%d.%d.%d", &a1, &a2, &a3, &a4 ) == 4)
-    {
+    if (sscanf(host.c_str(), "%d.%d.%d.%d", &a1, &a2, &a3, &a4 ) == 4) {
         sockaddr.sin_addr.s_addr =  inet_addr(host.c_str());
-    }
-    else {
+    } else {
         cout << "by name";
         hostent * record = gethostbyname(host.c_str());
         in_addr * addressptr = (in_addr *) record->h_addr;
         sockaddr.sin_addr = *addressptr;
     }
-    if(connect(sock,(struct sockaddr *)&sockaddr,sizeof(struct sockaddr))==-1)
-    {
+    if(connect(sock,(struct sockaddr *)&sockaddr,sizeof(struct sockaddr))==-1) {
         perror("connection fail");
         exit(1);
         return -1;
@@ -48,6 +49,7 @@ int createConnection(string host, int port)
     return sock;
 }
 
+<<<<<<< HEAD
 string requestReply(int sock, string message)
 {
     char buffer[BUFFER_LENGTH];
@@ -56,6 +58,14 @@ string requestReply(int sock, string message)
     if (count > 0)
     {
         usleep(10000);
+=======
+string requestReply(int sock, string message) {
+    char buffer[BUFFER_LENGTH];
+    string reply;
+    int count = send(sock, message.c_str(), message.size(), 0);
+    if (count > 0) {
+        usleep(1000);
+>>>>>>> develop
         do {
             count = recv(sock, buffer, BUFFER_LENGTH-1, 0);
             buffer[count] = '\0';
@@ -65,15 +75,23 @@ string requestReply(int sock, string message)
     return buffer;
 }
 
+<<<<<<< HEAD
 int request(int sock, string message)
 {
+=======
+int request(int sock, string message) {
+>>>>>>> develop
     char buffer[BUFFER_LENGTH];
     string reply;
     return send(sock, message.c_str(), message.size(), 0);
 }
 
+<<<<<<< HEAD
 string reply(int sock)
 {
+=======
+string reply(int sock) {
+>>>>>>> develop
     string strReply;
     int count;
     char buffer[BUFFER_LENGTH];
@@ -86,8 +104,7 @@ string reply(int sock)
     return strReply;
 }
 
-int responseToPort(string response)
-{
+int responseToPort(string response) {
     int parenIndex = static_cast<int>(response.find("("));
     string parsedIP, strReply;
     uint16_t a, b, c, d, e, f, first,second;
@@ -104,8 +121,7 @@ int responseToPort(string response)
     return port;
 }
 
-string responseToIp(string response)
-{
+string responseToIp(string response) {
     int parenIndex = static_cast<int>(response.find("("));
     string parsedIP, strReply;
     int a1,a2,a3,a4;
@@ -120,13 +136,11 @@ string responseToIp(string response)
     return buffer;
 }
 
-int PASV(int sockpi)
-{
+int PASV(int sockpi) {
     string strReply = requestReply(sockpi, "PASV\r\n");
     return createConnection(responseToIp(strReply),responseToPort(strReply));
 }
-void LIST(int sockpi)
-{
+void LIST(int sockpi) {
   int sockdtp = PASV(sockpi);
   request(sockpi, "LIST /\r\n");
   cout << "Server response: " << reply(sockpi) << endl;
@@ -134,8 +148,7 @@ void LIST(int sockpi)
   request(sockdtp,"CLOSE \r\n");
   cout << "Server response: " << reply(sockpi) << endl;
 }
-void RETR(int sockpi)
-{
+void RETR(int sockpi) {
   string filename;
   cout << "Enter the name of the File you wish to retrieve" << endl;
   cin >> filename;
@@ -146,25 +159,29 @@ void RETR(int sockpi)
   request(sockdtp,"CLOSE \r\n");
   cout << "Server response: " << reply(sockpi) << endl;
 }
-void QUIT(int sockpi)
-{
+void QUIT(int sockpi) {
     cout << requestReply(sockpi, "QUIT\r\n");
 }
+<<<<<<< HEAD
 int main(int argc , char *argv[])
 {
     int sockpi,sockdtp;
+=======
+int main(int argc , char *argv[]) {
+    int sockpi, sockdtp;
+>>>>>>> develop
     string strReply;
     string myinput;
 
     //TODO  arg[1] can be a dns or an IP address using gethostbyname.
-    if (argc > 2)
-    {
+    if (argc > 2){
         sockpi = createConnection(argv[1], atoi(argv[2]));
     }
-    if (argc == 2)
+    if (argc == 2){
         sockpi = createConnection(argv[1], 21);
-    else
+    } else {
         sockpi = createConnection("130.179.16.134", 21);
+    }
     strReply = reply(sockpi);
     cout << strReply  << endl;
 
@@ -182,23 +199,18 @@ int main(int argc , char *argv[])
 
     cout << "Please enter a command: (ls,passive,quit,get)" << endl;
 
-    while(true)
-    {
+    while (true) {// I'm not sure this is suppose to be like this
         cin >> myinput;
         //LIST
-        if(myinput == "ls")
-        {
+        if (myinput == "ls") {
             LIST(sockpi);
-        }
-        //RETR
-        else if(myinput == "get")
-        {
+        } else if (myinput == "get") {//RETR
             RETR(sockpi);
-        }
-        else if(myinput == "quit")
-        {
+        } else if(myinput == "quit") {
             QUIT(sockpi);
             return 0;
+        } else {
+            cout <<"Please enter a command(ls,passive,quit,get): Once, More..."<< endl;
         }
     }
 }
