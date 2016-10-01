445f62a1e9ab60d2d64f3075f46df0c4a551a5de
jadex1 <greatwolf3d@gmail.com>
2016-10-01 08:21:04 -0700

    Merged in changes

--- assignment2/Chat.java
+++ assignment2/Chat.java
@@ -7,9 +7,8 @@ import java.util.*;
   * using flooding
   **********************************/
 public class Chat implements Serializable {
-  public int pred;// predecessor, the node after.
-  public int succ;// succecessor, the node before.
-  public String text;
+  private int pred;// might need to be node
+  private int succ;// might need to be node
   public enum enum_MSG {
     JOIN,     // 0
     ACCEPT,   // 1
@@ -17,10 +16,26 @@ public class Chat implements Serializable {
     PUT,      // 3
   };
 
+  /*****************************//**
+  * \class Message class "chat.java"
+  * \brief LEAVE: Id_pred, Port_pred, IP_pred
+  **********************************/
+  void leave(String idPred, String portPred, int ipPred) {
+    //TODO: Fill in here.
+  }
+  /*****************************//**
+  * \class Message class "chat.java"
+  * \brief PUT: idSender, idDest, payload
+  **********************************/
+  void put(int senderId, int destId, String someText) {
+    //TODO: Fill in here.
+  }
   public class Message implements Serializable {
+    public enum_MSG msgid;
     public int portSrc;
     public int portDest;
-    public enum_MSG msgid;
+    public String text;
+    public Boolean fromInput;
   }
   /*****************************//**
   * \class Server class "chat.java"
@@ -31,12 +46,11 @@ public class Chat implements Serializable {
     //  String id;
     int intialPort;
     public Server(int p) {//Server takes a port only
-      //NOTE: I start the app out and I tell this instance of the app what port it starts on.
-      // This is the value for initialPort. it will not change through the lifecycle
-      // of the app.
+      //set the r
       System.out.println("The Server method was called and was assigned to port: "+p);
       this.pred = p;// this instances local variable.
       intialPort = p;
+      //this.id = id;
     }
     /*****************************//**
     * \class Message class "chat.java"
@@ -44,14 +58,13 @@ public class Chat implements Serializable {
     **********************************/
     public void joinAnotherServer(int port) {
       this.succ = port;
-      System.out.println("");
+      System.out.println("The messagee join method was called.");
     }
     /*****************************//**
     * \class Message class "chat.java"
     * \brief ACCEPT: Id_pred, Port_pred, IP_pred
     **********************************/
     public void acceptAnotherServer(String idPred, String portPred, int ipPred) {
-      // change the pred and succ methods here.
       //TODO: Fill in here.
     }
     public void sendMsgToNode(Message m, int toPort){
@@ -67,6 +80,12 @@ public class Chat implements Serializable {
         System.out.println("[Send MSG] IO: " + e.getMessage());
         e.printStackTrace();
       }
+    }
+    public void join(int toPort){
+
+    }
+    public void accept(int port){
+
     }
     public void run(){
       //System.out.println("The Server Run method was called.");
