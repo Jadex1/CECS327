854d40d8048cfa370a2e4cd86ef587c9c24550c4
jadex1 <greatwolf3d@gmail.com>
2016-10-23 21:36:50 -0700

    Still trying to get the md5 to work

--- assignment3/Chord/ChordUser.java
+++ assignment3/Chord/ChordUser.java
@@ -2,6 +2,8 @@ import java.rmi.*;
 import java.net.*;
 import java.util.*;
 import java.io.*;
+import java.math.*;
+import java.security.MessageDigest;
 public class ChordUser{
   int port;
   public ChordUser(int p){
@@ -42,7 +44,17 @@ public class ChordUser{
                 // this is either a known number or a created number.
                 int guid = Integer.parseInt(tokens[1]);
                 String thingOfaKey = Integer.toString(guid);
-                System.out.println(thingOfaKey);
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                md.update(thingOfaKey.getBytes());
+                byte byteData[] = md.digest();
+                //convert the byte to hex format method 1
+                StringBuffer sb = new StringBuffer();
+                for (int i = 0; i < byteData.length; i++) {
+                  sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+                }
+                System.out.println("Digest(in hex format):: " + sb.toString());
+                //System.out.println("MD5: "+ new BigInteger(1, m.digest()).toString(16));
+
                 // If you are using windows you have to use
                 path = ".\\"+  port +"\\"+Integer.parseInt(tokens[1]); // path to file
   			        //path = "./"+  port +"/"+guid; // path to file
